043feb4a6ccc7903ce5a0a3090809c363827f54b
bangjaehun <qkdwogns98@gmail.com>
2020-08-19 21:39:20 +0900

    pregress

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-const r = React;
-const rd = ReactDOM;
+const r = require("react");
+const rd = require("react-dom");
+
 const e = r.createElement;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -7,5 +8,5 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function App() {
-  return e("div", {}, "testing");
+  return e("div", { className: "App" }, "testing");
 }
