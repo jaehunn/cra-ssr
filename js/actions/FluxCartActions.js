c7cf7924e3176404b0af5692ffe3f4cae8b669d2
bangjaehun <qkdwogns98@gmail.com>
2020-08-20 14:15:29 +0900

    Dispatcher action attach

--- js/actions/FluxCartActions.js
+++ js/actions/FluxCartActions.js
@@ -1,16 +1,44 @@
 const AppDispatcher = require("../dispatcher/AppDispatcher");
 const ActionTypes = require("../constants/FluxCartConstants");
 
+// create payload -> throw dispatcher
 const Actions = {
-  receiveProduct: function () {},
+  receiveProduct: function (data) {
+    AppDispatcher.handleAction({
+      actionType: ActionTypes.RECEIVE_DATA,
+      data,
+    });
+  },
 
-  selectProduct: function () {},
+  selectProduct: function (data) {
+    AppDispatcher.handleAction({
+      actionType: ActionTypes.SET_SELECTED,
+      data,
+    });
+  },
 
-  addToCart: function () {},
+  //sku: stock keeping unit
+  addToCart: function (sku, update) {
+    AppDispatcher.handleAction({
+      actionType: ActionTypes.CART_ADD,
+      sku,
+      update,
+    });
+  },
 
-  removeFromCart: function () {},
+  removeFromCart: function (sku) {
+    AppDispatcher.handleAction({
+      actionType: ActionTypes.CART_REMOVE,
+      sku,
+    });
+  },
 
-  updateCartVisible: function () {},
+  updateCartVisible: function (cartVisible) {
+    AppDispatcher.handleAction({
+      actionType: ActionTypes.CART_VISIBLE,
+      cartVisible,
+    });
+  },
 };
 
 module.exports = Actions;
--- js/dispatcher/AppDispatcher.js
+++ js/dispatcher/AppDispatcher.js
@@ -2,6 +2,7 @@ const Dispatcher = require("flux").Dispatcher;
 
 const AppDispatcher = new Dispatcher();
 
+// action: actionType + payload
 AppDispatcher.handleAction = function (action) {
   this.dispatch({
     source: "VIEW_ACTION",
