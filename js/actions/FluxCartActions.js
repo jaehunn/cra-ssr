db3a032b0d5dc11a68268246196f397b266f7ad7
bangjaehun <qkdwogns98@gmail.com>
2020-08-20 14:00:10 +0900

    Create Dispatcher, Actions, Action Type

---
+++ js/actions/FluxCartActions.js
@@ -0,0 +1,16 @@
+const AppDispatcher = require("../dispatcher/AppDispatcher");
+const ActionTypes = require("../constants/FluxCartConstants");
+
+const Actions = {
+  receiveProduct: function () {},
+
+  selectProduct: function () {},
+
+  addToCart: function () {},
+
+  removeFromCart: function () {},
+
+  updateCartVisible: function () {},
+};
+
+module.exports = Actions;
---
+++ js/dispatcher/AppDispatcher.js
@@ -0,0 +1,12 @@
+const Dispatcher = require("flux").Dispatcher;
+
+const AppDispatcher = new Dispatcher();
+
+AppDispatcher.handleAction = function (action) {
+  this.dispatch({
+    source: "VIEW_ACTION",
+    action,
+  });
+};
+
+module.exports = AppDispatcher;
